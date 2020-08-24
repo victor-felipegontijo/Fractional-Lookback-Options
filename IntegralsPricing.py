be5c43d69817470e043f22b89c5f9eab2c9791c6
Victor <victorfgontijo@gmail.com>
2020-08-24 16:04:05 +0200

    updates_

--- IntegralsPricing.py
+++ IntegralsPricing.py
@@ -1,58 +1,58 @@
 from math import sqrt, exp, log, erf, erfc
 
 # I_1
-def I_1_1 (a, sigma, p , l, T):
+def I_1_1 (a, sigma, p , l, tau):
 
-    f1 = exp( (2*a + sigma)*(2*p + sigma*T)/2 )
-    f2 = 1 + erf( (p - l + T*(a + sigma))/sqrt(2*T) )
+    f1 = exp( (2*a + sigma)*(2*p + sigma*tau)/2 )
+    f2 = 1 + erf( (p - l + tau*(a + sigma))/sqrt(2*tau) )
 
     return f1*f2
 
-def I_1_2 (a, sigma, p , l, T):
+def I_1_2 (a, sigma, p , l, tau):
 
-    f_ext = exp( -(2*a + sigma)*(a*T -p) )/(2 + sigma/a)
-    s1 = exp( T*(2*a + sigma)*(2*a + sigma)/2)*(1 + erf( (-l + p + T*(a + sigma))/sqrt(2*T)) )
-    s2 = exp( (2*a + sigma)*(l - p + a*T) )*erfc( (l - p + a*T)/sqrt(2*T) )
+    f_ext = exp( -(2*a + sigma)*(a*tau -p) )/(2 + sigma/a)
+    s1 = exp( tau*(2*a + sigma)*(2*a + sigma)/2)*(1 + erf( (-l + p + tau*(a + sigma))/sqrt(2*tau)) )
+    s2 = exp( (2*a + sigma)*(l - p + a*tau) )*erfc( (l - p + a*tau)/sqrt(2*tau) )
 
     return f_ext*(s1 - s2)
 
-def I_1 (a, sigma, p , l, T):
+def I_1 (a, sigma, p , l, tau):
     
-    return I_1_1(a, sigma, p , l, T) - I_1_2(a, sigma, p , l, T)
+    return I_1_1(a, sigma, p , l, tau) - I_1_2(a, sigma, p , l, tau)
 
 # I_2
-def I_2_1 (a, p , l, T):
+def I_2_1 (a, p , l, tau):
 
-    return 0.5*(1 - erf((a*T - l - p)/sqrt(2*T)))
+    return 0.5*(1 - erf((a*tau - l - p)/sqrt(2*tau)))
 
-def I_2_2 (a, p , l, T):
+def I_2_2 (a, p , l, tau):
 
     f_ext = exp(2*a*l)
     
-    return 0.5*f_ext*(1 - erf((a*T + l - p)/sqrt(2*T)))
+    return 0.5*f_ext*(1 - erf((a*tau + l - p)/sqrt(2*tau)))
 
-def I_2 (a, p , l, T):
+def I_2 (a, p , l, tau):
     
-    return I_2_1(a, p, l, T) - I_2_2(a, p , l, T)
+    return I_2_1(a, p, l, tau) - I_2_2(a, p , l, tau)
 
 #I_3
-def I_3_1 (a, sigma, p , l, T):
+def I_3_1 (a, sigma, p , l, tau):
 
-    f_ext = exp(sigma*T*(2*a + sigma)/2)
+    f_ext = exp(sigma*tau*(2*a + sigma)/2)
 
-    return 0.5*f_ext*(1 - erf((T*(a + sigma) - l - p)/sqrt(2*T)))
+    return 0.5*f_ext*(1 - erf((tau*(a + sigma) - l - p)/sqrt(2*tau)))
 
-def I_3_2 (a, sigma, p , l, T):
+def I_3_2 (a, sigma, p , l, tau):
 
-    f_ext = exp(sigma*T*(2*a + sigma)/2 + 2*p*(a+sigma))
+    f_ext = exp(sigma*tau*(2*a + sigma)/2 + 2*p*(a+sigma))
 
-    return 0.5*f_ext*(1 + erf((T*(a + sigma) - l + p)/sqrt(2*T)))
+    return 0.5*f_ext*(1 + erf((tau*(a + sigma) - l + p)/sqrt(2*tau)))
 
-def I_3 (a, sigma, p , l, T):
+def I_3 (a, sigma, p , l, tau):
 
-    return I_3_1(a, sigma, p , l, T) + I_3_2(a, sigma, p , l, T)
+    return I_3_1(a, sigma, p , l, tau) + I_3_2(a, sigma, p , l, tau)
 
-# Put price
+# put price
 def FLB_Put_Pricing_Integrals (r, q, sigma, S_t, M_t, beta, tau):
     
     a = (r - q)/sigma - sigma/2
